ccba02ec5fdf2745fefee3b8dcf05c678d6cfb33
TofikE124 <eleltofik@gmail.com>
2024-07-08 19:34:06 +0300

    fix safari auto zoom input

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
+
       <body className={plus_jakarta_sans.className}>
         <Suspense>
           <Providers>
